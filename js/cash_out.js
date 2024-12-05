d9e8e260ad7ca891bf85ceff4e7969a2b2dc2c17
joynul24 <joynula919@gmail.com>
2024-12-05 10:52:38 +0600

    added padding and margin

---
+++ js/cash_out.js
@@ -0,0 +1,9 @@
+document.getElementById('btn-cash-out').addEventListener('click', function (event) {
+    event.preventDefault();
+
+    // const inputAmount = getConvertInputValue('input-cash-out');
+    // const pinNumber = getConvertInputValue('input-cash-out-pin');
+    
+    // const balance = getDivValueToNumber('balance')   
+
+});
